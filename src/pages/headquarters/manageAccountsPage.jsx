eaece7f2c8872e8e1016310452d3f8553b2db0ba
samuelchisale11 <chisales899@gmail.com>
2026-04-05 00:41:14 -0800

    i have applied some few changes

---
+++ src/pages/headquarters/manageAccountsPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ManageAccounts from '../../components/headquaeters/ManageAccounts/manageAccounts'
+
+function ManageAccountsPage() {
+  return (
+    <div className="h-screen w-screen overflow-hidden m-2 p-10">
+        <ManageAccounts />
+    </div>
+  )
+}
+
+export default ManageAccountsPage
