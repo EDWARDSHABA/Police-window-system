afe7c08ca335dcc447b2d85f99637dd4e9006f24
Felix Juma <bsc-inf-14-22@unima.ac.mw>
2026-04-09 21:06:15 +0200

    Upgrading

--- src/components/headquaeters/CreatePoliceStation/createPoliceStation.jsx
+++ src/components/headquaeters/CreatePoliceStation/createPoliceStation.jsx
@@ -223,11 +223,13 @@ Police Window System Team`;
   };
 
   return (
-    <div className="bg-gray-100 h-screen overflow-y-auto p-6 pt-15">
+    <div className="bg-gray-100 min-h-screen flex flex-col overflow-hidden p-6 pt-15 scrollbar-hidden">
       <Header />
 
+      <div className="flex-1 overflow-y-auto">
+
       {/* banner */}
-      <div className="bg-blue-600 text-white p-2 rounded-md mb-4 shadow">
+      <div className="bg-blue-300 text-white p-2 rounded-md  mb-4 shadow">
         <h2 className="text-lg font-regula">
           You can Create Police station and assign admin to manage the police station <br/>.
           <br/>
@@ -401,6 +403,7 @@ Police Window System Team`;
             </button>
           </div>
         </form>
+      </div>
       </div>
 
       <Footer />
