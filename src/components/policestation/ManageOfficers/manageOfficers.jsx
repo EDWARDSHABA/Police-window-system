bba2bb08907565d4022168046a0d432d784b52a0
Felix Juma <bsc-inf-14-22@unima.ac.mw>
2026-04-19 18:08:57 +0200

    WIP: update officer management and edit officer

--- src/components/policestation/ManageOfficers/manageOfficers.jsx
+++ src/components/policestation/ManageOfficers/manageOfficers.jsx
@@ -70,7 +70,7 @@ export default function ManageOfficers() {
         {/* Top Bar */}
         <div className="flex justify-between items-center mb-4">
           <button className="bg-blue-500 text-white px-4 py-2 rounded">
-            Manage Officer Accounts
+            Officers Accounts
           </button>
 
           <button
