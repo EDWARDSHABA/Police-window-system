f0bd9d2f312f91c96214eab404b05e511fdaed86
Victor5Muhakeya <muhakeyahvictor@gmail.com>
2026-04-20 16:31:26 +0200

    preapring to do my changes

--- src/components/officer/UpdateCase/updateCase.jsx
+++ src/components/officer/UpdateCase/updateCase.jsx
@@ -9,7 +9,7 @@ export default function UpdateCase() {
 
       <div className="flex-1 flex items-center justify-center pt-16">
         <div className="bg-white shadow-xl rounded-2xl p-10 text-center">
-          <h1 className="text-3xl font-bold text-yellow-700 mb-4">
+          <h1 className="text-3xl font-bold text-blue-300 mb-4">
             Update Case
           </h1>
           <p className="text-gray-600 text-lg">
